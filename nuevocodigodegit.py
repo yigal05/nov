5fbc6f4019b3dcf222f5821537cc592c937b295c
Yigal Rojas <yigalacevedo@gmail.com>
2022-01-22 19:34:50 -0500

    creo una interfaz y una funcion que permite reelegir el texto

--- nuevocodigodegit.py
+++ nuevocodigodegit.py
@@ -1,5 +1,5 @@
 #this code will do a small random parrafe
-import random as rd
+import random as rd,tkinter as tk
 parrafo_uno="""Sofía había ido a hacer algunas compras 
 cuando se encontró con Rocío, su prima y amiga.
 Juntas decidieron ir a la tienda de ropas para ver las
@@ -9,7 +9,18 @@ parrafo_dos="""Cerca de las 13 horas, y cuando ya el comercio
  había comprado aquellas cosas que su madre le había 
  encargado esa mañana.
 """
+def denuevo():
+	xd=rd.choice(parrafo)
+	texto["text"]=xd
+	texto["bg"]=rd.choice(["red","blue","cyan","fuchsia","yellow"])
 parrafo=[]
 parrafo.append(parrafo_uno)
 parrafo.append(parrafo_dos)
-print(rd.choice(parrafo))
+xd=rd.choice(parrafo)
+print(xd)
+raiz=tk.Tk()
+texto=tk.Label(text=xd,bg="red")
+texto.pack()
+boton=tk.Button(text="cambiar texto",command=denuevo).pack()
+raiz.config(background="red")
+raiz.mainloop()
